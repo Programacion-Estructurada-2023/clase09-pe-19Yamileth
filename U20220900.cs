7568f1b55effc171137dab30250038400cf2a6ca
19Yamileth <yg3717088@gmail.com>
2023-08-27 19:31:53 -0600

    Clade09

---
+++ U20220900.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MyApp// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+
+        {
+
+         //Ejercicio clase 9
+		
+            //Ejercicio clase 9
+		//Programa para evaluar la nota ingresada
+            Console.WriteLine("\n------Programa para evaluar la nota ingresada------\n");
+            Console.WriteLine("Ingresa la calificación: ");
+            double calificación = Convert.ToDouble(Console.ReadLine());
+   
+         //Si la nota es mayor o igual que 7.0
+         if (calificación >= 7)
+         {
+            Console.WriteLine("\nLa nota " + calificación + " ingresada es buena\n");
+                Console.WriteLine("\n------Fin del Programa------\n");
+         }
+
+         //Si la nota es mayor o igual que 8.5
+            else if (calificación >= 8.5)
+             {
+                Console.WriteLine("\nLa nota " + calificación + " ingresada es muy buena\n");
+                Console.WriteLine("\n------Fin del Programa------\n");
+            }
+
+            //Si la nota es mayor que 9.5
+            else if (calificación >= 9.5) 
+            { 
+                Console.WriteLine("\nLa nota " + calificación + " ingresada es excelente\n");
+                Console.WriteLine("\n------Fin del Programa------\n");
+            }
+
+                 //Si no se cumplen ninguna condición
+            else
+            {
+                Console.WriteLine("\nLa calificación " + calificación + " es deficiente\n");
+                Console.WriteLine("\n------Fin del Programa------\n");
+
+           }
+        }
+    }
+}
